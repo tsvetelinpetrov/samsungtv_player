bf2b59e642f84b3a793a0c536e2a5de3d2288348
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2022-06-23 23:13:20 +0300

    Checking for movies file existence

--- asp_web/asp_web/Services/MoviesDAO.cs
+++ asp_web/asp_web/Services/MoviesDAO.cs
@@ -6,6 +6,7 @@ namespace asp_web.Services
     public static class MoviesDAO
     {
         static List<MovieModel> items = new List<MovieModel>();
+        private static string MOVIES_FILE_PATH = AppDomain.CurrentDomain.BaseDirectory + "movies.json";
 
         static MoviesDAO()
         {
@@ -14,7 +15,12 @@ namespace asp_web.Services
 
         public static void loadJson()
         {
-            using (StreamReader r = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "movies.json"))
+            if(!File.Exists(MOVIES_FILE_PATH))
+            {
+                saveJson();
+            }
+
+            using (StreamReader r = new StreamReader(MOVIES_FILE_PATH))
             {
                 string json = r.ReadToEnd();
                 items = JsonConvert.DeserializeObject<List<MovieModel>>(json);
@@ -23,7 +29,7 @@ namespace asp_web.Services
 
         public static void saveJson()
         {
-            using (StreamWriter file = File.CreateText(AppDomain.CurrentDomain.BaseDirectory + "movies.json"))
+            using (StreamWriter file = File.CreateText(MOVIES_FILE_PATH))
             {
                 JsonSerializer serializer = new JsonSerializer();
                 serializer.Serialize(file, items);
